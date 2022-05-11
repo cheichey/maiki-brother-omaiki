dfe7df9525ae0163e3e3fe93cbfaf3b12365f681
Keisuke Sakamoto <66987550+cheichey@users.noreply.github.com>
2022-05-11 14:31:31 +0900

    feat: ステータスとアクティビティを設定する機能を追加

--- src/bot/bot.controller.ts
+++ src/bot/bot.controller.ts
@@ -1,9 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+import { InjectDiscordClient, On } from '@discord-nestjs/core';
+import { Client } from 'discord.js';
 
 @Controller('/bot')
 export class BotController {
+  constructor(
+    @InjectDiscordClient()
+    private readonly client: Client,
+  ) {}
+
   @Get()
   getBot() {
     return 'GET /bot succeed';
+  }
+
+  @On('ready')
+  setStatus() {
+    this.client.user.setStatus('idle');
+    this.client.user.setActivity({
+      name: 'マイキーとコストコに行ってるお！まいきーです。',
+    });
   }
 }
