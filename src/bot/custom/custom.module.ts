0b8a4da3598fc2bf3676224b7eb8cee5a0033fdb
Keisuke Sakamoto <66987550+cheichey@users.noreply.github.com>
2022-05-11 17:01:45 +0900

    fix: /now を module に追加した

--- src/bot/custom/custom.module.ts
+++ src/bot/custom/custom.module.ts
@@ -5,6 +5,7 @@ import { StartCommand } from './commands/start.command';
 import { HelpCommand } from './commands/help.command';
 import { FinishCommand } from './commands/finish.command';
 import { RandomCommand } from './commands/random.command';
+import { NowCommand } from './commands/now.command';
 @Module({
   imports: [DiscordModule.forFeature()],
   providers: [
@@ -13,6 +14,7 @@ import { RandomCommand } from './commands/random.command';
     HelpCommand,
     FinishCommand,
     RandomCommand,
+    NowCommand,
   ],
 })
 export class CustomModule {}
