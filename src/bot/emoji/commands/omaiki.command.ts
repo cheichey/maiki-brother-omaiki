54e4295d995bb041b7d7aa0baa5a4417adef3700
Keisuke Sakamoto <66987550+cheichey@users.noreply.github.com>
2022-05-12 15:06:30 +0900

    fix: /omaiki にガードを付けた

--- src/bot/emoji/commands/omaiki.command.ts
+++ src/bot/emoji/commands/omaiki.command.ts
@@ -8,9 +8,11 @@ import { CommandInteraction } from 'discord.js';
 import { EmojiService } from '../emoji.service';
 import { Injectable } from '@nestjs/common';
 import { options } from '../../options';
+import { GuildCommandGuard } from '../../guards/guild-command.guard';
 
 @Injectable()
 @Command(options.omaiki)
+@Command(GuildCommandGuard)
 export class OmaikiCommand implements DiscordTransformedCommand<any> {
   constructor(private readonly emojiService: EmojiService) {}
   handler(
